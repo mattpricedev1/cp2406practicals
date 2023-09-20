d5b1f4c665149cec37e669bc46ebb778d9b003a6
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-09-20 15:46:45 +1000

    Paste prac02 original code

--- prac02/prac02_prog01/prac02_prog01.cpp
+++ prac02/prac02_prog01/prac02_prog01.cpp
@@ -1,3 +1,32 @@
 //
 // Created by Matthew Price on 20/9/2023.
 //
+import<iostream>;
+import<format>;
+import employee;
+
+using namespace std;
+
+int main() {
+    // create and populate an employee
+    Employee anEmployee;
+    anEmployee.firstInitial = 'J';
+    anEmployee.lastInitial = 'D';
+    anEmployee.employeeNumber = 42;
+    anEmployee.salary = 80000;
+
+    // output the values of an employee
+    cout << format("Employee: {}{}", anEmployee.firstInitial, anEmployee.lastInitial) << endl;
+    cout << format("Number: {}", anEmployee.employeeNumber) << endl;
+    cout << format("Salary: ${}", anEmployee.salary) << endl;
+}
+
+export module
+employee;
+
+export struct Employee {
+    char firstInitial;
+    char lastInitial;
+    int employeeNumber;
+    int salary;
+};
