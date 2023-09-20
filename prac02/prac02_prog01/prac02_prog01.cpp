1484147f089c14f4a4cefee4b415c8ec07672039
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-09-20 17:16:12 +1000

    Revert back to single output

--- prac02/prac02_prog01/prac02_prog01.cpp
+++ prac02/prac02_prog01/prac02_prog01.cpp
@@ -1,6 +1,7 @@
 //
 // Created by Matthew Price on 20/9/2023.
 //
+
 #include <iostream>
 
 using namespace std;
@@ -18,7 +19,6 @@ namespace HR {
 int main() {
     // Create and populate an employee
     HR::Employee anEmployee;
-    HR::Employee employees[3];
 
     // Assign employee values
     anEmployee.firstInitial = 'J';
@@ -26,32 +26,8 @@ int main() {
     anEmployee.employeeNumber = 42;
     anEmployee.salary = 80000;
 
-    // Assign multiple employees using array
-    employees[0].firstInitial = 'M';
-    employees[0].lastInitial = 'P';
-    employees[0].employeeNumber = 96;
-    employees[0].salary = 120000;
-
-    employees[1].firstInitial = 'A';
-    employees[1].lastInitial = 'B';
-    employees[1].employeeNumber = 12;
-    employees[1].salary = 75000;
-
-    employees[2].firstInitial = 'C';
-    employees[2].lastInitial = 'D';
-    employees[2].employeeNumber = 34;
-    employees[2].salary = 123456;
-
-
     // Output the values of an employee
     cout << "Employee: " << anEmployee.firstInitial << anEmployee.lastInitial << endl;
     cout << "Number: " << anEmployee.employeeNumber << endl;
     cout << "Salary: $" << anEmployee.salary << endl;
-
-    // Output employees from array
-    for(auto & employee : employees) {
-        cout << "Employee: " << employee.firstInitial << employee.lastInitial << endl;
-        cout << "Number: " << employee.employeeNumber << endl;
-        cout << "Salary: $" << employee.salary << endl << "\n";
-    }
 }
