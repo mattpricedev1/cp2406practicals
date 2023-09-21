df8c7beb64c833930e99f8dc0a1da327ffcd7dda
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-09-21 16:20:25 +1000

    Add I/O functionality

--- prac03/prac03_task01.cpp
+++ prac03/prac03_task01.cpp
@@ -1,3 +1,14 @@
 //
 // Created by Matthew Price on 21/9/2023.
 //
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string userInput;
+    cout << "Please enter two strings: ";
+    getline(cin, userInput);
+    cout << userInput << endl;
+}
