1111b713fec340351a91f15e7c1e8a0217e75512
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-09-21 16:55:53 +1000

    Complete task 1

--- prac03/prac03_task01.cpp
+++ prac03/prac03_task01.cpp
@@ -2,13 +2,26 @@
 // Created by Matthew Price on 21/9/2023.
 //
 #include <iostream>
-#include <string>
+/*
+ * You do not need to #import <string> because it instead uses C-style strings.
+ * - They are built-in to the language.
+ */
 
 using namespace std;
 
 int main() {
-    string userInput;
-    cout << "Please enter two strings: ";
-    getline(cin, userInput);
-    cout << userInput << endl;
+    string userString01, userString02;
+
+    cout << "Please first string: ";
+    getline(cin, userString01);
+
+    cout << "Please second string: ";
+    getline(cin, userString02);
+
+    // Process strings in alphabetical order and output result.
+    if(userString01 < userString02) {
+        cout << userString01 << " " << userString02 << endl;
+    } else {
+        cout << userString02 << " " << userString01 << endl;
+    }
 }
