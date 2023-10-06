50658ec281f0b35a2b45f2b5673876426874aad1
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-10-06 14:16:10 +1000

    Add prac 03 task 03

---
+++ prac03/prac03_task03.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Matthew Price on 6/10/2023.
+//
+#include <iostream>
+
+using namespace std;
+
+/* Question: Why is the following line here?
+ *           - Unlike Python, the function 'findAndReplace' must be declared before main.
+ *           - The function definition of 'findAndReplace' is written below main.
+ */
+string findAndReplace(string haystack, const string& needle, const string& replaceWith);
+
+
+int main() {
+
+    string haystack{"THIS IS A TEST TEXT"};
+    string needle{"TEST"};
+    string replaceWith{"???"};
+
+    string result = findAndReplace(haystack, needle, replaceWith);
+
+    cout << "haystack: " << haystack << endl;
+    cout << "needle: " << needle << endl;
+    cout << "replaceWith: " << replaceWith << endl;
+    cout << "result: " << result << endl;
+
+}
+
+string findAndReplace(string haystack, const string& needle, const string& replaceWith) {
+    // Make a copy of haystack
+    string result = std::move(haystack);
+
+    auto indexPosition(result.find(needle));
+    if (indexPosition != string::npos) {
+        result.replace(indexPosition, needle.length(), replaceWith);
+    }
+    return result;
+}
