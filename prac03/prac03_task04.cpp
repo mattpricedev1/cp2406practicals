cc14746b6c3e8e35dd3ac55cf0124f37fe9607ce
mattpricedev1 <matthew.price1@my.jcu.edu.au>
2023-10-06 15:22:04 +1000

    Complete prac 03 task 04

--- prac03/prac03_task04.cpp
+++ prac03/prac03_task04.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Matthew Price on 6/10/2023.
 //
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<double> numbers{};  // Initialise empty vector
+
+    cout << "Enter an unknown number of floating-point numbers (followed by enter)\n"
+            "To stop, enter 0:\n";
+
+    while(true) {
+        double number{};
+        cout << ">>> ";
+        cin >> number;
+        if(number != 0) {
+            numbers.insert(numbers.end(), number);  // Insert number at end of vector
+        } else {
+            break;
+        }
+    }
+
+    for(double number : numbers) {
+        cout << number << endl;
+    }
+}
